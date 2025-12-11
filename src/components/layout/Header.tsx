4d939ad01530540c20f2c0da0b986f6258b78be0
ns-morozova <ns_morozova@mail.ru>
2025-12-11 19:35:04 +0300

    Updated header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -12,18 +12,16 @@ const Header = () => {
     <ul
       className={
         variant === "desktop"
-          ? "flex items-center gap-13 text-xs"
+          ? "flex items-center gap-13 text-xs leading-[140%] tracking-[0.09em]"
           : "flex flex-col gap-4 py-4 text-xs"
       }
     >
       {NAV_LINKS.map((item) => {
        
-
         return (
-          <li key={item.href}>
+          <li key={item.href} className="uppercase transition-colors duration-200 hover:text-accent-aqua">
             <Link
               href={item.href}
-              className="uppercase"
               onClick={variant === "mobile" ? () => setIsMenuOpen(false) : undefined}
             >
               {item.label}
@@ -36,19 +34,30 @@ const Header = () => {
 
   return (
     <header className="sticky top-0 z-50 bg-black">
-      <div className="mx-auto flex max-w-8xl items-end justify-between px-5 py-7.5 md:px-9 md:py-9">
-        <Link href="/">
-          <Image
-            src="/logo.svg"
-            alt="MRNet"
-            width={185}
-            height={41}
-            priority
-            className="h-auto w-40 md:w-46"
-          />
-        </Link>
+      <div className="mx-auto flex max-w-8xl items-end justify-between px-5 py-5 md:px-9 md:py-9">
+        <div className="flex flex-col gap-3.5 xl:flex-row xl:items-end xl:gap-14">
+          <Link href="/">
+            <Image
+              src="/logo.svg"
+              alt="MRNet"
+              width={185}
+              height={41}
+              priority
+              className="h-auto w-40 md:w-46"
+            />
+          </Link>
+          <span className="text-accent-aqua text-xs leading-[140%]">+ 7 (800) 600-35-38</span>
+        </div>
+        
 
-        <nav className="hidden lg:block">{renderLinks("desktop")}</nav>
+        {/* <nav className="hidden lg:block">{renderLinks("desktop")}</nav> */}
+
+        <div className="hidden lg:flex lg:flex-col lg:items-end lg:gap-3.5 lg:mb-1.5 xl:flex-row xl:gap-13 xl:mb-0">
+          <nav>{renderLinks("desktop")}</nav>
+          <span className="uppercase text-accent-aqua text-xs leading-[140%] tracking-[0.09em]">
+            Оставить заявку
+          </span>
+        </div>
 
         <button
           type="button"
